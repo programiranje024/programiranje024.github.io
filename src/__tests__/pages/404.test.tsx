8ce85111fd14f68860a92921a7da6911bda231dc
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2023-03-30 02:58:47 +0200

    fix: fix 404 test

--- src/__tests__/pages/404.test.tsx
+++ src/__tests__/pages/404.test.tsx
@@ -6,7 +6,7 @@ describe('404', () => {
   it('renders a heading', () => {
     render(<NotFoundPage />);
 
-    const heading = screen.getByText(/not found/i);
+    const heading = screen.getByText(/nije pronađena/i);
 
     expect(heading).toBeInTheDocument();
   });
