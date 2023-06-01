bf31a0b124154332a89dc1c8707e0ef21d23d973
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-06-01 02:28:33 +0200

    fix: fix useLayoutEffect ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from 'next/app';
+import React from 'react';
 
 import '@/styles/globals.css';
+
+if (!process.browser) {
+  React.useLayoutEffect = () => {
+    return;
+  };
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
